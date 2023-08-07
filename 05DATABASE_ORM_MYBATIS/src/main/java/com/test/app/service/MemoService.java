14dd4283d0da4a855e4c48b9173e58cbcc3b881e
seongjin0902 <ksj05099@naver.com>
2023-08-07 15:28:13 +0900

    0807

---
+++ 05DATABASE_ORM_MYBATIS/src/main/java/com/test/app/service/MemoService.java
@@ -0,0 +1,37 @@
+package com.test.app.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.test.app.domain.dto.MemoDto;
+import com.test.app.domain.mapper.MemoMapper;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+@Service
+@Slf4j
+public class MemoService {
+	
+	@Autowired
+	private MemoMapper mapper;
+	
+//	전체메모 가져오기
+	public List<MemoDto> getAllMemo(){
+		log.info("MemoService's getAllMemo 콜~!");
+		return mapper.selectAll();
+	}
+
+	public int addMemo(MemoDto dto) {
+		log.info("MemoService's getAllMemo 콜~!");
+		return mapper.insert(dto);
+	}
+	
+	
+//	메모 작성
+//	메모 수정
+//	메모 삭제
+
+}
