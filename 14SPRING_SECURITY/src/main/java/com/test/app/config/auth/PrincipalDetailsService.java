6ccf0549445d22f47a04bbe68fa75c71f968e494
seongjin0902 <ksj05099@naver.com>
2023-08-17 10:46:54 +0900

    0817

--- 14SPRING_SECURITY/src/main/java/com/test/app/config/auth/PrincipalDetailsService.java
+++ 14SPRING_SECURITY/src/main/java/com/test/app/config/auth/PrincipalDetailsService.java
@@ -1,15 +1,30 @@
 package com.test.app.config.auth;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.test.app.domain.dto.UserDto;
+import com.test.app.domain.mapper.UserMapper;
 
 public class PrincipalDetailsService implements UserDetailsService {
+	
+	@Autowired
+	private UserMapper userMapper;
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+		
+		UserDto dto = userMapper.selectAt(username);
+		if(dto == null)
+			return null;
+		
+		return new;
 	}
 
 }
